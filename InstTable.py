39a6a970d91c4ef3230e4be9141a2070a945e961
dbwlsl97 <kimyjin0920@gmail.com>
2018-07-09 20:29:14 +0900

    pass1 하는 중

--- InstTable.py
+++ InstTable.py
@@ -1,34 +1,39 @@
 class InstTable:
 
-    def __init__(self):
+    def __init__(self, fileName):
         self.instDic = {}
+        self.openFile(fileName)
 
     def openFile(self, fileName):
         file = open(fileName, 'r')
-        lines = file.readlines()
+        lines = file.readlines( )
         for line in lines:
-            inst = line.split()[0]
-            self.instDic[inst] = line.split()[1:]
-            Instruction.parsing(self, line)
-        file.close()
+            #            inst = line.split()[0]
+            inst = Instruction(line)
+
+            self.instDic[inst.instruction] = inst
+
+        #            self.instDic[inst.instruction] = line.split()[1:]
+        file.close( )
+
 
 class Instruction:
-    def __init__(self):
-        # self.instruction = instruction
-        # self.opcode = opcode
-        # self.numberOfoperand = numberOfoperand
-        # self.format = format
+    def __init__(self, line):
+
+        self.i_token = None
         self.instruction = ""
         self.opcode = None
         self.numberOfoperand = None
         self.format = None
-        self.parsing()
+        self.parsing(line)
 
-    def parsing(self,line):
-        self.instruction = line.split()[0]
-        if line.split()[1] == "3/4" :
+    def parsing(self, line):
+        i_token = line.split( )
+        self.instruction = i_token[0]
+
+        if i_token[1] == "3/4":
             self.format = 3
-        else :
-            self.format = line.split()[1]
-        self.opcode = line.split()[2]
-        self.numberOfoperand = line.split()[3]
+        else:
+            self.format = i_token[1]
+        self.opcode = i_token[2]
+        self.numberOfoperand = i_token[3]
